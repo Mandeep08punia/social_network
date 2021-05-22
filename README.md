ef2e05552979a331c239cac6197c406b18652d85
Mandeep Punia <48849295+Mandeep08punia@users.noreply.github.com>
2021-05-22 12:30:08 +0530

    social-network

---
+++ README.md
@@ -0,0 +1,4 @@
+# Social Network
+
+### Tech Stack- MERN 
+A MERN stack social network application with MVP features implemented: user authentication, profile viewing, posting, commenting, and following/unfollowing.
---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,23 @@
+
+// https://appdividend.com/2018/07/18/react-redux-node-mongodb-jwt-authentication/#React_Redux_Node_MongoDB_JWT_Authentication
+
+import { SET_CURRENT_USER } from '../actions/actionTypes';
+import isEmpty from '../validation/isEmpty';
+
+const initialState = {
+  isAuthenticated: false,
+  user: {}
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload
+      };
+    default:
+      return state;
+  }
+}
